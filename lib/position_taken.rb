35795f3da7732bf66d5633b0a4385c7e11a93e93
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-05 18:55:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, index, value="X")
+ if board[index] == " "
+   return false
+ end
+ 
+ if board[index] == ""
+   return false
+ end
+ 
+ if board[index] == nil
+   return false
+ end
+   
+     
+end
